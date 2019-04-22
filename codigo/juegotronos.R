fbbb9d3ff91e78910e1986d68bc7f65c5b8029ce
Patricia Loto <39300655+PatriLoto@users.noreply.github.com>
2019-04-22 13:03:39 -0300

    Graficos mejorados
    Total de personajes por género por casa noble
    Total de personajes por género por casa noble excepto los que no pertencen a ninguna.

--- codigo/juegotronos.R
+++ codigo/juegotronos.R
@@ -1,9 +1,9 @@
 # Install packages
 install.packages("wesanderson")
 install.packages(readr)
-install.packages("viridis")
-install.packages("highcharter")
-library(highcharter)
+#install.packages("viridis")
+#install.packages("highcharter")
+#library(highcharter)
 library (tidyverse)
 library (readxl)
 library(RColorBrewer)
@@ -12,11 +12,11 @@ library(plotly)
 library(wesanderson)
 
 # ---------------------------------------------------------------------------------------------------------------- 
-# Lectura del dataset 1
+# Lectura del dataset 1: tiempo_pantalla
 tiempo_pantalla <- readr::read_csv("https://raw.githubusercontent.com/cienciadedatos/datos-de-miercoles/master/datos/2019/2019-04-17/tiempo_pantalla.csv")
 View(tiempo_pantalla)
 # ---------------------------------------------------------------------------------------------------------------- 
-#limpieza inicial
+#limpieza inicial del dataset 1
 tiempo_pantallaSinNA <-na.omit(tiempo_pantalla)
 View(tiempo_pantallaSinNA)
 tiempo_pantallalimpio <-na.omit(tiempo_pantalla)
@@ -25,10 +25,10 @@ View(tiempo_pantallalimpio)
 # filtro los primeros 10 personajes con mmás minutos en pantalla
 primeros10 <- filter(tiempo_pantallaSinNA, (tiempo_pantallaSinNA$minutos_pantalla>103))
 View(primeros10)
-#ver
+#ver cómo hacer el ordenamiento
 #ordeno <-tiempo_pantallaSinNA %>% order_by(tiempo_pantallaSinNA,ascendente) 
 # ---------------------------------------------------------------------------------------------------------------- 
-#ggplot - Gráfico original
+#ggplot - Exploración de datos: primer gráfico
 # ---------------------------------------------------------------------------------------------------------------- 
 p<- ggplot(data=primeros10, aes(x=nombre, y=minutos_pantalla), colour=nombre) + geom_col(aes(fill=nombre))
 p +  labs(x = "Personajes", y = "Minutos en Pantalla", colour = " Personajes", 
@@ -60,12 +60,11 @@ p +  labs(x = "Personajes", y = "Minutos en Pantalla", colour = " Personajes",
                                   color="black", #Color del t?tulo: maroon, lightblue, lightblue,darkblue, darkorange, black.
                                   lineheight=2.0)) + scale_fill_discrete(name="Personajes")
 # ---------------------------------------------------------------------------------------------------------------- 
-# ---------------------------------------------------------------------------------------------------------------- 
-#ggplot - Correcto para publicar con más minutos en pantalla
+# ggplot - Correcto para publicar con mayor cantidad de minutos en pantalla con colores por defecto
 # ---------------------------------------------------------------------------------------------------------------- 
 p<- ggplot(data=primeros10, aes(x=nombre, y=minutos_pantalla), colour=nombre) + geom_col(aes(fill=nombre))
 p +  labs(x = "Personajes", y = "Minutos en Pantalla", colour = " Personajes", 
-          title = "Personajes con más minutos en pantalla",
+          title = "Personajes con mayor cantidad de minutos en pantalla",
           subtitle = "Temporada de 1 a 6")+  # xlab=""
   theme_classic()+
   theme (axis.text.x = element_blank())+
@@ -78,22 +77,22 @@ p +  labs(x = "Personajes", y = "Minutos en Pantalla", colour = " Personajes",
                                   color="black", #Color del t?tulo: maroon, lightblue, lightblue,darkblue, darkorange, black.
                                   lineheight=2.0),  legend.position= "", legend.text = element_blank())
 # ---------------------------------------------------------------------------------------------------------------- 
-#ggplot - Correcto para publicar con menos minutos en pantalla con paleta Darjeeling1
+#  Procesamiento previo para del segundo gráfico: menor cantidad de minutos en pantalla
 # ---------------------------------------------------------------------------------------------------------------- 
-
-# filtro los ultimos 10
+#Verifico los últimos valores del dataset
 tail(tiempo_pantallaSinNA)
 #ultimos <- top_n(1,10,minutos_pantalla) %>% order_by(minutos_pantalla)
-#View (ultimos)
+# filtro los ultimos 10
 ultimos10 <- filter(tiempo_pantallaSinNA, (tiempo_pantallaSinNA$minutos_pantalla<2.30))%>% arrange(ascendente(tiempo_pantallaSinNA))
 View(ultimos10)
 
 # ---------------------------------------------------------------------------------------------------------------- 
-# con paleta de con paleta de WesAnderson
+# PUBLICADO - ggplot con menor cantidad de minutos en pantalla con paleta Darjeeling2
+# ---------------------------------------------------------------------------------------------------------------- 
 pal <- wes_palette("Darjeeling2", 10, type = "continuous")
 p<- ggplot(data=ultimos10, aes(x=nombre, y=minutos_pantalla), colour=nombre) + geom_col(aes(fill=nombre))
 p +  labs(x = "Personajes", y = "Tiempo (min)", colour = " Personajes", 
-          title = "Personajes con menos minutos en pantalla",
+          title = "Personajes con menor cantidad de minutos en pantalla",
           subtitle = "Temporada de 1 a 6")+ 
   scale_fill_manual(values=pal)+ scale_x_discrete(limits=c("Ilyn Payne","Ternesio Terys","Quaithe", "Hugh of the Vale", "Rorge",
                                                              "Tickler", "Mhaegen","Biter", "Lhara", "Tobho Mott"))+
@@ -109,12 +108,12 @@ p +  labs(x = "Personajes", y = "Tiempo (min)", colour = " Personajes",
                                   color="black", #Color del t?tulo: maroon, lightblue, lightblue,darkblue, darkorange, black.
                                   lineheight=2.0),  legend.position= "", legend.text = element_blank())
 # ---------------------------------------------------------------------------------------------------------------- 
-#ggplot - Correcto para publicar con menos minutos en pantalla con paleta color_brewer
+# opcion 2: ggplot con menor cantidad de minutos en pantalla con paleta color_brewer (Correcto para publicar)
 # ----------------------------------------------------------------------------------------------------------------
 #opcion 2 con scale_color_brewer(palette = "Dark2")
 p<- ggplot(data=ultimos10, aes(x=nombre, y=minutos_pantalla), colour=nombre) + geom_col(aes(fill=nombre))
 p +  labs(x = "Personajes", y = "Tiempo (min)", colour = " Personajes", 
-          title = "Personajes con menos minutos en pantalla",
+          title = "Personajes con menor cantidad de minutos en pantalla",
           subtitle = "Temporada de 1 a 6")+ 
   scale_color_brewer(palette = "Dark2")+ scale_x_discrete(limits=c("Ilyn Payne","Ternesio Terys","Quaithe", "Hugh of the Vale", "Rorge",
                                                            "Tickler", "Mhaegen","Biter", "Lhara", "Tobho Mott"))+
@@ -122,15 +121,15 @@ p +  labs(x = "Personajes", y = "Tiempo (min)", colour = " Personajes",
   theme (axis.text.x = element_blank())+
   theme(axis.text.x = element_text(angle = 50, vjust = 1.1, hjust=1, color="black"))+  #los meses se visualizan en forma vertical en el eje x
   theme(plot.title = element_text(family="Courier",
-                                  size=rel(1.5),        #Tama?o relativo de la letra del t?tulo
+                                  size=rel(1.5),       
                                   vjust=2, 
-                                  hjust= 0.5,     #Justificación del título, permite centrarlo
+                                  hjust= 0.5,           #Justificación del título, permite centrarlo
                                   position_identity(center),   
-                                  face="bold",        #Tipo: Letra negra, otras posibilidades son "plain", "italic", "bold" y "bold.itali
-                                  color="black", #Color del t?tulo: maroon, lightblue, lightblue,darkblue, darkorange, black.
+                                  face="bold",          #Tipo: Letra negra, otras posibilidades son "plain", "italic", "bold" y "bold.itali
+                                  color="black",        #Color del t?tulo: maroon, lightblue, lightblue,darkblue, darkorange, black.
                                   lineheight=2.0),  legend.position= "", legend.text = element_blank())
 # ---------------------------------------------------------------------------------------------------------------- 
-#ggplot - Correcto para publicar en Tablero con más minutos en pantalla, con paleta Rushmore1 de WesAnderson
+# PUBLICADO - ggplot: con mayor cantidad de minutos en pantalla, con paleta Rushmore1 de WesAnderson
 # ---------------------------------------------------------------------------------------------------------------- 
 pal <- wes_palette("Rushmore1", 10, type = "continuous")  #preferidos: Rushmore1,GrandBudapest1, Darjeeling2
 p<- ggplot(data=primeros10, aes(x=nombre, y=minutos_pantalla), colour=nombre) + geom_col(aes(fill=nombre))+
@@ -151,16 +150,9 @@ p<- ggplot(data=primeros10, aes(x=nombre, y=minutos_pantalla), colour=nombre) + 
                                   color="darkred", #Color del t?tulo: maroon, lightblue, lightblue,darkblue, darkorange, black.
                                   lineheight=2.0), legend.position= "", legend.text = element_blank())
 p
-funciona <-ggplotly(p)+theme(plot.title = element_text(family="Courier",
-                                                       size=rel(1.5),        #Tama?o relativo de la letra del t?tulo
-                                                       vjust=2, 
-                                                       hjust= 0.5, #Justificaci?n vertical, para separarlo del gr?fico
-                                                       position_identity(center),   
-                                                       face="bold",        #Tipo: Letra negra, otras posibilidades son "plain", "italic", "bold" y "bold.itali
-                                                       color="darkred", #Color del t?tulo: maroon, lightblue, lightblue,darkblue, darkorange, black.
-                                                       lineheight=2.0), legend.position= "", legend.text = element_blank())
-funciona
+
 # ---------------------------------------------------------------------------------------------------------------- 
+# Formateo de los índices para el gráfico de plotly
 yaxis <- list(title = "USD (millones)",
               showline = TRUE,
               showgrid = FALSE,
@@ -196,7 +188,7 @@ grafico <- plot_ly (x = ~(primeros10$nombre), y = ~(primeros10$minutos_pantalla)
 grafico
   
 # ---------------------------------------------------------------------------------------------------------------- 
-# Lectura del dataset 2
+# Lectura del dataset 2: personaje_libros
 # ---------------------------------------------------------------------------------------------------------------- 
 personajes_libros <- readr::read_csv("https://raw.githubusercontent.com/cienciadedatos/datos-de-miercoles/master/datos/2019/2019-04-17/personajes_libro.csv")
 View(personajes_libros)
@@ -205,18 +197,30 @@ View(genero)
 # agrupa por lealtad y cuenta la cantidad de hombres y mujeres
 generoporlealtad <-personajes_libros %>% group_by(lealtad) %>% count(genero)                 
 View (generoporlealtad)
-#renombro
-names (generoporlealtad) =c("lealtad", "genero","total")
+# A los que no tienen ninguna lealtad reempazo por sin Lealtad
+generoporlealtad[generoporlealtad$lealtad == "Ninguna",1]<-"No pertenede a ninNo pertenece a ninguna casa noble"
+# Renombro columnas
+names (generoporlealtad) =c("lealtad", "genero","totalXgenero")
 View(generoporlealtad)
+# Selecciono los que tienen casa Noble, es decir, elimino los que no tienen Lealtad.
+datosconCasaNoble <- generoporlealtad %>% filter(lealtad!="Sin Lealtad")  
+View(datosconCasaNoble)
+
+# Probar
+#calculo el total de personajes para poder calcular luego el porcentaje
+#TotalPersonajes <- (generoporlealtad %>% summarize(total=sum(generoporlealtad$totalXgenero)))
+#View (TotalPersonajes)
+
+#porcen<-generoporlealtad %>% ((generoporlealtad$totalXgenero/917)*100)
 # ---------------------------------------------------------------------------------------------------------------- 
-# Grafico para publicar en el tablero
+# Grafico para publicar en el tablero 
 # ---------------------------------------------------------------------------------------------------------------- 
-pal
-ggplot(generoporlealtad, aes(x=lealtad,y=total, colour=genero))+
+# Cantidad de personaje por géneros por casa Noble, se excluyen aquellos que no la tienen
+ggplot(datosconCasaNoble, aes(x=lealtad,y=totalXgenero, colour=genero))+
 geom_col(aes(fill=genero),width = .4)+
   #scale_fill_manual(values =c("purple", "lightblue")) +
   #scale_fill_manual(values=pal)+
-  labs (x = "Lealtad", y = "Cantidad por género")+
+  labs (title= "Cantidad de personaje por géneros por casa Noble", x = "Lealtad", y = "Cantidad por género")+
   theme(axis.text.x = element_text(angle = 50, vjust = 1, hjust=1))+  #los meses se visualizan en forma vertical en el eje x
   theme(plot.title = element_text(family="Courier",
                                   size=rel(1.5),        #Tama?o relativo de la letra del t?tulo
@@ -228,7 +232,7 @@ geom_col(aes(fill=genero),width = .4)+
 # ---------------------------------------------------------------------------------------------------------------- 
 #plotly - # Grafico para publicar en el tablero
 # ---------------------------------------------------------------------------------------------------------------- 
-plot_ly (x = generoporlealtad$lealtad, y = generoporlealtad$total, color = generoporlealtad$lealtad, text = paste('Personaje','', generoporlealtad$lealtad,":",generoporlealtad$total,sep=""), 
+plot_ly (x = generoporlealtad$lealtad, y = generoporlealtad$totalXgenero, color = generoporlealtad$lealtad, text = paste('Lealtad','', generoporlealtad$lealtad,":",generoporlealtad$totalXgenero, sep=""), 
          hoverinfo = "text", type = "bar") %>% layout(xaxis = list(showline = F, 
                                                showticklabels = F, 
                                                fixedrange = T,
@@ -239,7 +243,7 @@ plot_ly (x = generoporlealtad$lealtad, y = generoporlealtad$total, color = gener
 # ---------------------------------------------------------------------------------------------------------------- 
 #plotly
 # ----------------------------------------------------------------------------------------------------------------  
-p<- plot_ly (x = generoporlealtad$lealtad, y = generoporlealtad$total, color = generoporlealtad$genero, text = paste('Lealtad','',generoporlealtad$lealtad,":",generoporlealtad$total), 
+p<- plot_ly (x = generoporlealtad$lealtad, y = generoporlealtad$totalXgenero, color = generoporlealtad$genero, text = paste('Lealtad','',generoporlealtad$lealtad,":",generoporlealtad$totalXgenero), 
          hoverinfo = "text", type = "bar") %>% layout(title= 'Temporada de la 1 a la 6', legend= 'Personaje',
                                                       xaxis = list(showline = F, 
                                                                    showticklabels = F, 
@@ -248,15 +252,15 @@ p<- plot_ly (x = generoporlealtad$lealtad, y = generoporlealtad$total, color = g
                                                                    title = "Lealtad"),
                                                       yaxis = list(fixedrange = T, 
                                                                    title = ""))
-
+p
 # ---------------------------------------------------------------------------------------------------------------- 
-# probar
+# Para probar y jugar
 donutdata <- personajes_libros %>% 
   group_by(lealtad) %>% 
   (generoXLea = count(genero))
 view(donutdata)
 
-hc <-hchart(generoporlealtad, "pie", hcaes(name = lealtad, y = total), innerSize = 300)
+hc <-hchart(generoporlealtad, "pie", hcaes(name = lealtad, y = totalXgenero), innerSize = 300)
 
 #
 p %>% 
@@ -313,8 +317,6 @@ gptres <- gpdos %>%
   group_by(provincia)  %>% 
   count(generoVictima)%>% 
 # ---------------------------------------------------------------------------------------------------------------- 
-
+# último dataset
 cambio_lealtades <- readr::read_csv("https://raw.githubusercontent.com/cienciadedatos/datos-de-miercoles/master/datos/2019/2019-04-17/cambio_lealtades.csv")
 View (cambio_lealtades)
-personajes_libros <- readr::read_csv("https://raw.githubusercontent.com/cienciadedatos/datos-de-miercoles/master/datos/2019/2019-04-17/personajes_libro.csv")
-View(personajes_libros)
