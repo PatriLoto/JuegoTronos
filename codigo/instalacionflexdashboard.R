e53899114172b5c186e484e2a4a6ed2670cf5452
Patricia Loto <39300655+PatriLoto@users.noreply.github.com>
2019-04-24 01:12:17 -0300

    Update instalacionflexdashboard.R

--- codigo/instalacionflexdashboard.R
+++ codigo/instalacionflexdashboard.R
@@ -1,2 +1,6 @@
+# 1.Debo instalar primero el paquete flexdashboard
+# 2. Luego, voy a File -> New file y selecciono la opción R Markdown
+# 3. Selecciono la opción "From Template", luego de los templates elijo "Flex Dashboard"
+# 4. Listo, puedo comenzar a armar el tablero
 install.packages("flexdashboard")
 library(flexdashboard)
